ca2e16289ae5ebb2cc0daf7969244427fa8d2948
Ashleigh Arnaud <ashleigh.arnaud@gmail.com>
2017-07-05 20:51:27 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,137 @@
+import React, { Component } from 'react'
+import mori from 'mori'
+
+// -----------------------------------------------------------------------------
+// Mori Component
+// -----------------------------------------------------------------------------
+
+// a MoriComponent receives a JavaScript Object with one key: imdata
+// imdata should be a mori structure that supports mori.equals() comparisons
+class MoriComponent extends Component {
+  // only update the component if the mori data structure is not equal
+  shouldComponentUpdate (nextProps, _nextState) {
+    return !mori.equals(this.props.imdata, nextProps.imdata)
+  }
+}
+
+// -----------------------------------------------------------------------------
+// Square
+// -----------------------------------------------------------------------------
+
+function clickSquare (rowIdx, colIdx) {
+  const currentState = window.CURRENT_STATE
+  const currentColor = mori.get(currentState, 'brushColor')
+  const newState = mori.assocIn(currentState, ['board', rowIdx, colIdx], currentColor)
+  window.NEXT_STATE = newState
+}
+
+class Pixel extends MoriComponent {
+  render () {
+    const color = mori.get(this.props.imdata, 'color')
+    const rowIdx = mori.get(this.props.imdata, 'rowIdx')
+    const colIdx = mori.get(this.props.imdata, 'colIdx')
+    let className = 'square'
+
+    const clickFn = mori.partial(clickSquare, rowIdx, colIdx)
+
+    const pixelStyle = {
+      backgroundColor: color
+    }
+
+    return (
+      <div className={className} onClick={clickFn} style={pixelStyle} />
+    )
+  }
+}
+
+// -----------------------------------------------------------------------------
+// Board
+// -----------------------------------------------------------------------------
+
+class Row extends MoriComponent {
+  render () {
+    // const board = mori.get(this.props.imdata, 'board')
+    const colVec = mori.get(this.props.imdata, 'cols')
+    const numCols = mori.count(colVec)
+    const rowIdx = mori.get(this.props.imdata, 'rowIdx')
+
+    let squares = []
+    for (let colIdx = 0; colIdx < numCols; colIdx++) {
+      let color = mori.get(colVec, colIdx)
+      let pixelData = mori.hashMap('rowIdx', rowIdx, 'colIdx', colIdx, 'color', color)
+      let key = 'pixel-' + rowIdx + '-' + colIdx
+
+      squares.push(<Pixel imdata={pixelData} key={key} />)
+    }
+
+    return (
+      <div className='row'>{squares}</div>
+    )
+  }
+}
+
+function clickColorBtn (newColor) {
+  const currentState = window.CURRENT_STATE
+  const newState = mori.assoc(currentState, 'brushColor', newColor)
+  window.NEXT_STATE = newState
+}
+
+function ColorPickerButton (color) {
+  return <button onClick={clickColorBtn.bind(null, color)} className={color}></button>
+}
+
+function morilog (cljThing) {
+  console.log(mori.toJs(cljThing))
+}
+
+class ColorPicker extends MoriComponent {
+  render () {
+    const colorsVec = this.props.imdata
+    const numColors = mori.count(colorsVec)
+
+    // morilog(colorsVec)
+
+    let colorButtons = []
+    for (let i = 0; i < numColors; i++) {
+      let color = mori.get(colorsVec, i)
+      colorButtons.push(ColorPickerButton(color))
+    }
+    return (
+      <div className='colors'>{colorButtons} </div>
+    )
+  }
+}
+
+function clickResetBtn () {
+  window.NEXT_STATE = mori.hashMap('board', window.EMPTY_BOARD)
+}
+
+function App (props) {
+  const boardVec = mori.get(props.imdata, 'board')
+  const numRows = mori.count(boardVec)
+  const colorBrush = mori.get(props.imdata, 'brushColor')
+
+  let rows = []
+  for (let rowIdx = 0; rowIdx < numRows; rowIdx++) {
+    let colVec = mori.get(boardVec, rowIdx)
+    let rowData = mori.hashMap('cols', colVec, 'rowIdx', rowIdx)
+    let key = 'row-' + rowIdx
+
+    rows.push(<Row imdata={rowData} key={key} />)
+  }
+
+  const colorsVec = mori.get(props.imdata, 'colors')
+
+  return (
+    <div className='app-container'>
+      <h1>Ashleigh's Paint</h1>
+      <div className='board'>{rows}</div>
+      <div className='colors-container'>
+        <ColorPicker imdata={colorsVec} />
+      </div>
+      <button onClick={clickResetBtn}>Undo</button>
+    </div>
+  )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,87 @@
+import ReactDOM from 'react-dom'
+import App from './App'
+import mori from 'mori'
+import './index.css'
+
+// -----------------------------------------------------------------------------
+// Application State
+// -----------------------------------------------------------------------------
+
+const numRows = 50
+const numCols = 80
+
+function createEmptyBoard () {
+  var board = []
+  for (let i = 0; i < numRows; i++) {
+    board[i] = []
+
+    for (let j = 0; j < numCols; j++) {
+      board[i][j] = false
+    }
+  }
+
+  return board
+}
+
+window.EMPTY_BOARD = mori.toClj(createEmptyBoard())
+const colors = mori.vector('black', 'red', 'yellow', 'purple', 'blue', 'green')
+
+const initialState = {
+  board: window.EMPTY_BOARD,
+  colors: colors,
+  brushColor: mori.first(colors)
+}
+
+// CURRENT_STATE is always the current state of the application
+window.CURRENT_STATE = null
+
+// NEXT_STATE is the next state the application should be in
+// Start it off with a PDS version of our initialState object.
+window.NEXT_STATE = mori.toClj(initialState)
+
+let renderCount = 0
+
+// You can track each application state using a mori vector.
+// window.HISTORY = mori.vec()
+
+// -----------------------------------------------------------------------------
+// Render Loop
+// -----------------------------------------------------------------------------
+
+const rootEl = document.getElementById('root')
+
+// constantly render on every requestAnimationFrame
+function render () {
+  // Only trigger a render if CURRENT_STATE and NEXT_STATE are different.
+  // NOTE: checking deep equality of a persistent data structure is a fast and
+  //       cheap operation, even for large data structures
+  if (!mori.equals(window.CURRENT_STATE, window.NEXT_STATE)) {
+    // do not perform the update if NEXT_STATE is not valid
+    if (!isValidState(window.NEXT_STATE)) {
+      console.warn('Oops! Tried to set an invalid NEXT_STATE')
+      window.NEXT_STATE = window.CURRENT_STATE
+    } else {
+      // next state is now our current state
+      window.CURRENT_STATE = window.NEXT_STATE
+
+      // you might add this new state to your history vector here...
+      // window.HISTORY = mori.conj(window.HISTORY, window.CURRENT_STATE)
+
+      ReactDOM.render(App({imdata: window.CURRENT_STATE}), rootEl)
+
+      renderCount = renderCount + 1
+      // console.log('Render #' + renderCount)
+    }
+  }
+
+  window.requestAnimationFrame(render)
+}
+
+window.requestAnimationFrame(render)
+
+// this is a sanity-check function so you can ensure your state is valid
+function isValidState (state) {
+  return mori.isMap(state) &&
+         mori.isVector(mori.get(state, 'board'))
+         // TODO: add more conditions here as appropriate
+}
